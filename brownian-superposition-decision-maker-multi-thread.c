5b52ef28f2b46244658c3a1003a31ba2840bf54a
Tsubasa Kato <tsubasa@superai.online>
2023-11-09 22:22:07 +0900

    Update brownian-superposition-decision-maker-multi-thread.c

--- brownian-superposition-decision-maker-multi-thread.c
+++ brownian-superposition-decision-maker-multi-thread.c
@@ -6,9 +6,12 @@
 
 #define STEPS 1000
 #define NUM_THREADS 8
+
 //Created with the help of ChatGPT (GPT-4)
 //(C)Tsubasa Kato 11/5/2023 10:03AM - Inspire Search Corporation
 //Website: https://www.inspiresearch.io/en
+//Personal Search Engine with Web Crawler: https://www.growmysearch.com
+//Refer to the file bsdmt-ChatGPT-explanation.txt for explanation of this code by ChatGPT (GPT-4)
 pthread_mutex_t mutex_output;
 
 typedef struct {
@@ -16,20 +19,19 @@ typedef struct {
     double difficulty;
     double foreseen_future;
     double disturbance;
+    double final_x; // Store final x position
+    double final_y; // Store final y position
 } ThreadData;
 
-// Thread-safe random double generation
 double random_double(unsigned int *seedp) {
     return 2.0 * ((double)rand_r(seedp) / RAND_MAX) - 1.0;
 }
 
-// Weighted decision that is also thread-safe
 int weighted_decision(double weight, unsigned int *seedp) {
     double threshold = (1.0 - weight) * ((double)rand_r(seedp) / RAND_MAX);
     return threshold < 0.5 ? 1 : 0;
 }
 
-// Thread function for simulating Brownian motion
 void *simulate_brownian_motion(void *threadarg) {
     ThreadData *my_data = (ThreadData *) threadarg;
     int id = my_data->id;
@@ -58,7 +60,17 @@ void *simulate_brownian_motion(void *threadarg) {
         pthread_mutex_unlock(&mutex_output);
     }
 
+    // Store the final positions
+    my_data->final_x = x;
+    my_data->final_y = y;
+
     pthread_exit(NULL);
+}
+
+// Superposition function to combine two positions
+void superimpose_positions(double x1, double y1, double x2, double y2, double *super_x, double *super_y) {
+    *super_x = (x1 + x2) / 2.0; // Average of x positions
+    *super_y = (y1 + y2) / 2.0; // Average of y positions
 }
 
 int main() {
@@ -87,6 +99,15 @@ int main() {
     // Wait for all threads to complete
     for (t = 0; t < NUM_THREADS; t++) {
         pthread_join(threads[t], NULL);
+    }
+
+    // Apply superposition to combine positions
+    double super_x, super_y;
+    for (t = 0; t < NUM_THREADS - 1; t += 2) {
+        superimpose_positions(thread_data[t].final_x, thread_data[t].final_y,
+                              thread_data[t + 1].final_x, thread_data[t + 1].final_y,
+                              &super_x, &super_y);
+        printf("Superimposed Position of particles %ld and %ld: (%f, %f)\n", t, t + 1, super_x, super_y);
     }
 
     // Clean up and exit
